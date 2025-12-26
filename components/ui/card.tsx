74f97f006192cfc30809df44e1d8ec9c791ece99
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-26 12:42:44 -0300

    icons, components design update

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card flex flex-col gap-6 rounded-xl border py-6 text-black shadow-sm",
+        "bg-card flex flex-col gap-6 overflow-hidden rounded-[16px] border border-[#999999] pb-6 text-black",
         className
       )}
       {...props}
@@ -20,7 +20,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card-header"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-2 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        "bg-primary @container/card-header flex items-center justify-between gap-2 p-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
         className
       )}
       {...props}
@@ -32,7 +32,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
+      className={cn("text-[22px] leading-none font-bold text-white", className)}
       {...props}
     />
   )
