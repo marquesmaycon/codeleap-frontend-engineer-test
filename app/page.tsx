6a79838ec314c62d796be7fcaddaa30f335c9aab
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-26 12:42:53 -0300

    home page init

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,75 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+import EditIcon from "@/public/edit-pen.svg"
+import TrashIcon from "@/public/trash-bin.svg"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div className="mx-auto max-w-200">
+        <header className="bg-primary px-9.25 py-6.75">
+          <h1 className="text-xl font-bold text-white">CodeLeap Network</h1>
+        </header>
+        <div className="space-y-6 bg-white p-6">
+          <Card>
+            <CardContent className="space-y-6 pt-6">
+              <h2 className="text-[22px] font-bold">What’s on your mind?</h2>
+
+              <div className="space-y-2">
+                <Label htmlFor="title">Title</Label>
+                <Input id="title" placeholder="Hello world" />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="content">Content</Label>
+                <Textarea id="content" placeholder="Content here" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="ml-auto text-base">Create</Button>
+            </CardFooter>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle>My First Post at CodeLeap Network!</CardTitle>
+              <CardAction>
+                <Button size="icon-lg" variant="ghost">
+                  <TrashIcon className="size-7.5" />
+                </Button>
+                <Button size="icon-lg" variant="ghost">
+                  <EditIcon className="size-7.5" />
+                </Button>
+              </CardAction>
+            </CardHeader>
+            <CardContent>
+              <div className="mb-4 flex items-center justify-between">
+                <h3 className="text-lg font-bold text-[#777777]">@Maycon</h3>
+                <span className="text-right text-[#777777]">25 minutes ago</span>
+              </div>
+              <p className="whitespace-pre-line">
+                Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
+                egestas. Maecenas egestas arcu quis ligula mattis placerat.
+                {"\n"}
+                Duis vel nibh at velit scelerisque suscipit. Duis lobortis massa imperdiet quam.
+                Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin
+                urna dolor sagittis lacus. Fusce a quam. Nullam vel sem. Nullam cursus lacinia erat.
+              </p>
+            </CardContent>
+          </Card>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
-  );
+  )
 }
