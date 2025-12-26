86c2b262a8f6ec1b423229ee37cb9c4c213666b0
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-26 14:56:41 -0300

    route structures update

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "next/navigation"
+
+import { getUser } from "@/features/auth/actions"
+import SignUpForm from "@/features/auth/components/sign-up-form"
+
+export default async function SignUpPage() {
+  const username = await getUser()
+
+  if (username) {
+    redirect("/")
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center p-4">
+      <SignUpForm />
+    </div>
+  )
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Footer from "@/components/layout/footer"
+import Header from "@/components/layout/header"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
