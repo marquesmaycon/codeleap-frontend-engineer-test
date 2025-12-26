7172667b8a30849498cf1e83593662f969cc9f84
marquesmaycon <mayconmarquesh@gmail.com>
2025-12-26 11:41:29 -0300

    design configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  variable: "--font-serif",
   subsets: ["latin"],
+  weight: ['300', '400', '500', '700'],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} font-serif antialiased min-h-screen flex flex-col`}
       >
         {children}
       </body>
